8dd50b85c77413163e0279389103bb507694b540
Amrutha-Gopalakrishnan <amruthagopal16@gmail.com>
2025-05-20 21:34:08 +0530

    updated files

--- src/Education.jsx
+++ src/Education.jsx
@@ -70,7 +70,7 @@ import React, { useEffect } from "react";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import scool from './assets/scool.png' 
-import clg from './assets/clg.jped'
+import clg from './assets/clg.jpeg'
 
 const educationData = [
   {
