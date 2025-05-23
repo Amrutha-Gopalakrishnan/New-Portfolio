841ef223b428f6b628a299c2642cfe24ed05ac82
Amrutha-Gopalakrishnan <amruthagopal16@gmail.com>
2025-05-23 13:03:20 +0530

    Updated Files

--- src/Home.jsx
+++ src/Home.jsx
@@ -124,7 +124,7 @@ function Home() {
       <img
         src="./profile1.jpg"
         alt="profile"
-        className="w-40 h-40 sm:w-60 sm:h-60 md:w-65 md:h-65 lg:w-70 lg:h-70 rounded-3xl object-cover shadow-md"
+        className="sm:w-100 sm:h-100 md:w-65 md:h-65 lg:w-70 lg:h-70 rounded-3xl object-cover shadow-md"
       />
 
       <div className="text-center mt-6 w-full max-w-2xl">
